6316fce6c69524e87b4c1721c9b6a2efa5ea1c00
arouzing <593336a4-f160-432d-981d-34f51d9ad98d@anonaddy.me>
2022-02-26 11:50:53 -0500

    fixed commands and a big error

--- run.sh
+++ run.sh
@@ -3,10 +3,10 @@ USERNAME=auto
 
 curl -fsSL https://tailscale.com/install.sh | sh
 sleep 2
-sudo tailscale up --advertise-tags=provision --accept-routes
+sudo tailscale up --advertise-tags=tag:ansible --accept-routes
 
 sudo useradd $USERNAME
 sudo mkdir -p /home/$USERNAME/.ssh
-sudo sh -c "echo '$USERNAME ALL=(ALL:ALL) NOPASSWD:ALL' > /etc/sudoers"
+grep -q "echo '$USERNAME ALL=(ALL:ALL) NOPASSWD:ALL" /etc/sudoers || sudo sh -c "echo '$USERNAME ALL=(ALL:ALL) NOPASSWD:ALL' >> /etc/sudoers"
 
 curl -fsSL https://raw.githubusercontent.com/arouzing/mainframe/main/gh-actions.pub -o /home/$USERNAME/.ssh/authorized_keys
