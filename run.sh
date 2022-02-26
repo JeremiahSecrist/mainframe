fd3031e81d6f481a2634602415eae565f49a1132
arouzing <593336a4-f160-432d-981d-34f51d9ad98d@anonaddy.me>
2022-02-26 11:38:36 -0500

    improved init script

--- run.sh
+++ run.sh
@@ -1,8 +1,12 @@
 #!/usr/bin/sh
+USERNAME=auto  
+
 curl -fsSL https://tailscale.com/install.sh | sh
-sudo tailscale up --advertise-tags=tag:provis,gh
+sleep 2
+sudo tailscale up --advertise-tags=provision --accept-routes
 
-sudo useradd auto
-sudo 
+sudo useradd $USERNAME
+sudo mkdir -p /home/$USERNAME/.ssh
+sudo sh -c "echo '$USERNAME ALL=(ALL:ALL) NOPASSWD:ALL' > /etc/sudoers"
 
-curl -fsS: https://raw.githubusercontent.com/arouzing/mainframe/main/gh-actions.pub 
+curl -fsSL https://raw.githubusercontent.com/arouzing/mainframe/main/gh-actions.pub -o /home/$USERNAME/.ssh/authorized_keys
