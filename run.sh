187762de364514fb18e411ce79378f622782a9f1
arouzing <593336a4-f160-432d-981d-34f51d9ad98d@anonaddy.me>
2022-02-26 19:16:11 -0500

    fixed ssh

--- run.sh
+++ run.sh
@@ -1,12 +1,27 @@
 #!/usr/bin/sh
+
+# init varriables
 USERNAME=auto  
+USERPATH=/home/$USERNAME
 
-curl -fsSL https://tailscale.com/install.sh | sh
+# install tailscale
+which tailscale || curl -fsSL https://tailscale.com/install.sh | sh
 sleep 2
-sudo tailscale up --advertise-tags=tag:ansible --accept-routes
+sudo tailscale up --advertise-tags=tag:ansible --accept-routes >> tailscale.log &
 
+#create user with proper keys
 sudo useradd $USERNAME
 sudo mkdir -p /home/$USERNAME/.ssh
-grep -q "echo '$USERNAME ALL=(ALL:ALL) NOPASSWD:ALL" /etc/sudoers || sudo sh -c "echo '$USERNAME ALL=(ALL:ALL) NOPASSWD:ALL' >> /etc/sudoers"
+grep -q "$USERNAME ALL=(ALL:ALL) NOPASSWD:ALL" /etc/sudoers || sudo sh -c "echo '$USERNAME ALL=(ALL:ALL) NOPASSWD:ALL' >> /etc/sudoers"
 
+sudo chmod go-w $USERPATH || sudo mkdir -p $USERPATH ; sudo chmod go-w $USERPATH 
+# download public key for ssh 
 curl -fsSL https://raw.githubusercontent.com/arouzing/mainframe/main/gh-actions.pub -o /home/$USERNAME/.ssh/authorized_keys
+# permission verification
+sudo chown $USERNAME "$USERPATH/.ssh/authorized_keys"
+sudo chmod 700 "$USERPATH/.ssh"
+sudo chmod 600 "$USERPATH/.ssh/authorized_keys"
+sudo systemctly enable --now sshd
+
+# display logs from before
+cat ./tailscale.log
